fb9a8a228e427e12155ed60eb37e688b4146d877
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2018-09-19 11:41:55 -0400

    Lotta Changes
    Decided on CSS Grids
    Implemented basic grid layout
    Working on media queries for responsiveness

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -32,5 +32,7 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/PortfolioApp/')),
 ]
 
-# Use static() to add url mapping to serve static files during development (only)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    # Use static() to add url mapping to serve static files during development (only)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- PortfolioApp/models.py
+++ PortfolioApp/models.py
@@ -17,13 +17,14 @@ class PortfolioSection(models.Model):
 
 class Image(models.Model):
     # Model representing an image file contained in a portfolio section
-    image_url = models.CharField(max_length=200)
-    thumbnail_url = models.CharField(max_length=200)
+    # image_url = models.CharField(max_length=200)
+    # thumbnail_url = models.CharField(max_length=200)
     alt_text = models.CharField(max_length=200, blank=True)
     #priority = models.IntegerField(default=0)
-    width = models.IntegerField()
-    height = models.IntegerField()
+    width = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
     #portfolio_section = models.ManyToManyField(PortfolioSection, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/%d/', height_field='height', width_field='width', null=True)
 
     def __str__(self):
         return self.alt_text
