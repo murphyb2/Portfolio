c680044de0c5c9b650dd713dcc993d039ec7b761
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2018-09-22 16:36:46 -0400

    Updates to HTML/CSS
    Need to start Django dev

--- PortfolioApp/models.py
+++ PortfolioApp/models.py
@@ -17,14 +17,16 @@ class PortfolioSection(models.Model):
 
 class Image(models.Model):
     # Model representing an image file contained in a portfolio section
-    # image_url = models.CharField(max_length=200)
+    # ADD VALIDATION TO ENSURE THAT ASPECT RATIO IS 3x2 or 2x3 IF USING AS A HOME PAGE SHOWCASE IMAGE!!!!! 
+    picture = models.ImageField(upload_to='pictures/%Y/%m/%d/', height_field='height', width_field='width', null=True)
+    image_url = models.CharField(blank=True, null=True, max_length=200)
     # thumbnail_url = models.CharField(max_length=200)
-    alt_text = models.CharField(max_length=200, blank=True)
+    alt_text = models.CharField(max_length=200, null=True)
     #priority = models.IntegerField(default=0)
     width = models.IntegerField(blank=True, null=True)
     height = models.IntegerField(blank=True, null=True)
     #portfolio_section = models.ManyToManyField(PortfolioSection, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='pictures/%Y/%m/%d/', height_field='height', width_field='width', null=True)
+    is_showcase_image = models.BooleanField(default=False)
 
     def __str__(self):
         return self.alt_text
@@ -32,3 +34,14 @@ class Image(models.Model):
     def is_landscape(self):
         return self.width > self.height
 
+class Home(models.Model):
+    # Model representing the home page
+
+    # Array holding the showcase clickable images to link to each portfolio section
+    # showcase_images = []
+
+    title = models.CharField(default="Home", max_length=100)
+    # title = "home"
+
+    def __str__(self):
+        return self.title
