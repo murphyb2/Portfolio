615cff09f75430c53faa5ac22ffa4a4daa37dfec
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2018-09-25 19:09:02 -0400

    Changed to MySQl
    changed databse

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': ''
     }
 }
 
