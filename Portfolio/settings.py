32fcf2b0b5d22b5b78f9476316f11ebe4ea5128d
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2018-11-13 20:26:59 -0500

    Changed DB For Heroku

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'portfolio',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'portfolio',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'PORT': ''
+#     }
+# }
 
 
 # Password validation
