8fd8b19e677c1a77981632b30d2b9ab2ffc0277b
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2018-11-23 08:05:11 -0500

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'PortfolioApp.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'Portfolio.storage_backends.MediaStorage'
 print(STATIC_URL)
 
 # Static files (CSS, JavaScript, Images)
