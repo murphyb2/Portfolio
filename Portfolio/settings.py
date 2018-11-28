a433a0040c6e16f13c79ac7eee0d0ecbf535c7ae
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2018-11-27 21:25:54 -0500

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -140,7 +140,8 @@ AWS_DEFAULT_ACL = 'bucket-owner-full-control'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_REGION = os.environ.get('AWS_REGION')
+AWS_S3_CUSTOM_DOMAIN = 's3-%s.amazonaws.com/%s' % (AWS_REGION, AWS_STORAGE_BUCKET_NAME)
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
